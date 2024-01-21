a06151c1d5284baea8d4bffd51aaab313ba90878
Travis Hohl <travis@travishohl.com>
2024-01-21 10:15:35 -0600

    Create first class and test that it can be created.

---
+++ src/Basket.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace ThriftCartCodeChallenge;
+
+class Basket
+{
+    public function __construct()
+    {
+    }
+}
---
+++ tests/BasketTest.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use ThriftCartCodeChallenge\Basket;
+
+final class BasketTest extends TestCase
+{
+    public function testCanBeCreated(): void
+    {
+        $basket = new Basket();
+
+        $this->assertInstanceOf(Basket::class, $basket);
+    }
+}
